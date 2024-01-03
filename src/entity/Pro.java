1cc4c885ee81862b30489571ec1f50437974bef9
Challucas <108103759+Challucas@users.noreply.github.com>
2024-01-03 16:13:47 +0100

    Add files via upload

---
+++ src/entity/Pro.java
@@ -0,0 +1,21 @@
+package entity;
+
+public class Pro {
+
+	private String nomSociete;
+
+	
+	
+	public Pro(String nomSociete) {
+		this.nomSociete = nomSociete;
+	}
+
+	public String getNomSociete() {
+		return nomSociete;
+	}
+
+	public void setNomSociete(String nomSociete) {
+		this.nomSociete = nomSociete;
+	}
+	
+}
