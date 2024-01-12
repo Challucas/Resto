03c75bdf6df890af2e7706b2ae80bcb8b486ed7a
clementfellah <clement.fellah14@gmail.com>
2024-01-12 15:57:38 +0100

    Formulaire création de réservation

--- src/controller/CreateReservController.java
+++ src/controller/CreateReservController.java
@@ -1,6 +1,52 @@
-
 package controller;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class CreateReservController {
+
+    @FXML
+    private AnchorPane anchorMenu;
+
+    @FXML
+    private ImageView wallpaper;
+
+    @FXML
+    private ImageView btnParticulier;
+
+    @FXML
+    private ImageView btnPro;
+    
+
+    @FXML
+    void particulierForm(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/createReservParticulier.fxml"));
+        BorderPane root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void proForm(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/createReservPro.fxml"));
+        BorderPane root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
---
+++ src/controller/CreateReservFormController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class CreateReservFormController {
+
+    @FXML
+    private ImageView wallpaper;
+
+    @FXML
+    private DatePicker selectDate;
+
+    @FXML
+    private TextField inputNom;
+
+    @FXML
+    private TextField inputPrenom;
+
+    @FXML
+    private TextField inputTel;
+
+    @FXML
+    private TextField inputNbr;
+    
+    @FXML
+    private TextField inputSociete;
+
+
+}
