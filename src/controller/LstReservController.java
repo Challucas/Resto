d8a20468b036ff36656ab9b67eaca31699474f71
Challucas <lucas.chalscl@gmail.com>
2024-01-13 11:40:57 +0100

    Add popup on click on item

--- src/controller/LstReservController.java
+++ src/controller/LstReservController.java
@@ -5,11 +5,17 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import service.LstReservModele;
 
@@ -47,10 +53,43 @@ public class LstReservController implements Initializable{
 				if (post != null) {
 					this.listViewReservWeb.getItems().add(post);				}
 			}
+			
+			this.listViewReservWeb.setOnMouseClicked(new EventHandler<MouseEvent>() {
+	            @Override
+	            public void handle(MouseEvent event) {
+	                String selectedItem = (String) listViewReservWeb.getSelectionModel().getSelectedItem();
+	                if (selectedItem != null) {
+	                	displayPopup(selectedItem);
+	                }
+	            }
+	        });
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    }
+    
+    private void displayPopup(String selectedItem) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Détails de la réservation");
+        alert.setHeaderText(null);
+        alert.setContentText(selectedItem);
+
+        final ButtonType validerButton = new ButtonType("Valider");
+        final ButtonType refuserButton = new ButtonType("Refuser");
+
+        alert.getButtonTypes().setAll(validerButton, refuserButton);
+
+        alert.showAndWait().ifPresent(new Consumer<ButtonType>() {
+            @Override
+            public void accept(ButtonType buttonType) {
+                if (buttonType == validerButton) {
+                    
+                } else if (buttonType == refuserButton) {
+                    
+                }
+            }
+        });
     }
 
 }
