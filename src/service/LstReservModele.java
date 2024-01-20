a95897ca03674d992fe407627b296637ba30e00d
Challucas <lucas.chalscl@gmail.com>
2024-01-20 18:47:30 +0100

    Ajout lsite bdd + suppression reserv Web

--- src/service/LstReservModele.java
+++ src/service/LstReservModele.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import entity.*;
@@ -11,7 +15,15 @@ import entity.*;
 public class LstReservModele {
 	
     private ArrayList<String> listClient = new ArrayList<>();
+    private ArrayList<String> lstReserv = new ArrayList<>();
+    private Reservation lstReservClient = new Reservation();
 	
+    private Connection conn;
+    
+	public LstReservModele(Connect connect) {
+		this.conn = connect.conn;
+	}
+    
 	public ArrayList<String> getListClient() throws IOException {
 		File file = new File("src/assets/reservWeb/reserv.txt");
 		if (file.exists()) {
@@ -19,12 +31,12 @@ public class LstReservModele {
 				String lineRead;
 				while ((lineRead = reader.readLine()) != null) {
 					String[] parts = lineRead.split(";");
-					if (parts.length == 7 && parts[0].trim().equals("particulier")) {
-						String reservClientPart = parts[1] + parts[2]  + parts[3] + parts[4] + "per" + " service" + parts[6] + " le" + parts[5];
+					if (parts.length == 7 && parts[1].trim().equals("particulier")) {
+						String reservClientPart = parts[2] + " | " + parts[3]  + " | " + parts[4] + " | " + parts[5] + " |" + parts[6] + " | " + parts[0];
 						this.listClient.add(reservClientPart);
 					}
-					else if (parts.length == 6 && parts[0].trim().equals("Professionnel")) {
-						String reservClientPro = parts[1] + parts[2] + parts[3] + "per" + " service" + parts[5] + " le" + parts[4];
+					else if (parts.length == 6 && parts[1].trim().equals("Professionnel")) {
+						String reservClientPro = parts[2] + " | " + parts[3] + " | " + parts[4] +" | " + parts[5] + " | " + parts[0];
 						this.listClient.add(reservClientPro);
 					}
 				}
@@ -32,5 +44,81 @@ public class LstReservModele {
 			}
 		}
 		return null;
+	}
+	
+	public ArrayList<String> getListReservBDD() throws SQLException {
+        ArrayList<String> resultList = new ArrayList<>();
+        if (this.conn == null) {
+            return resultList;
+        }
+        
+        resultList.addAll(getPartBDD());
+        resultList.addAll(getProBDD());
+       
+        return resultList;
+    }
+	
+	
+	
+	public ArrayList<String> getPartBDD() throws SQLException {
+		ArrayList<String> resultListPart = new ArrayList<>();
+		if (this.conn == null) {
+			return resultListPart;
+		}
+		
+		String queryGetReserv = "SELECT res.id_reservation idReservation, par.prenom, par.nom, cli.telephone, res.nbr_personne, res.date " +
+				"FROM reservation res " +
+				"JOIN client cli ON res.id_client = cli.id_client " +
+				"JOIN type_client tcli ON cli.id_type_client = tcli.id_type_client " +
+				"JOIN particulier par ON tcli.id_pro_part = par.id_pro_part ";
+		
+		try (PreparedStatement pstReservation = this.conn.prepareStatement(queryGetReserv);
+				ResultSet resultSet = pstReservation.executeQuery()) {
+			
+			while (resultSet.next()) {
+				String resultRow = resultSet.getString("prenom") + " " +
+						resultSet.getString("nom") + " " +
+						resultSet.getString("telephone") + " " +
+						resultSet.getInt("nbr_personne") + " " +
+						resultSet.getDate("date");
+				
+				resultListPart.add(resultRow);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return resultListPart;
+	}
+	
+	public ArrayList<String> getProBDD() throws SQLException {
+		ArrayList<String> resultListPro = new ArrayList<>();
+		if (this.conn == null) {
+			return resultListPro;
+		}
+		
+		String queryGetReserv = "SELECT pro.nom_societe, cli.telephone, res.nbr_personne, res.date " +
+								"FROM reservation res " +
+								"JOIN client cli ON res.id_client = cli.id_client " +
+								"JOIN type_client tcli ON cli.id_type_client = tcli.id_type_client " +
+								"JOIN professionel pro ON tcli.id_pro_part = pro.id_pro_part";
+				
+		
+		try (PreparedStatement pstReservation = this.conn.prepareStatement(queryGetReserv);
+				ResultSet resultSet = pstReservation.executeQuery()) {
+			
+			while (resultSet.next()) {
+				String resultRow = resultSet.getString("nom_societe") + " " +
+						resultSet.getString("telephone") + " " +
+						resultSet.getInt("nbr_personne") + " " +
+						resultSet.getDate("date");
+				
+				resultListPro.add(resultRow);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return resultListPro;
 	}
 }
--- src/service/ReservationModel.java
+++ src/service/ReservationModel.java
@@ -1,8 +1,15 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.StringTokenizer;
 
 import entity.Reservation;
 
@@ -27,5 +34,51 @@ public class ReservationModel {
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public void deleteReservationBDD() {
+	}
+	
+	
+
+	public void deleteReservationWeb(String selectedItem) {
+	    String[] champs = selectedItem.split("\\s*\\|\\s*");
+
+	    if (champs.length == 6 || champs.length == 5) {
+	        int idToDelete = Integer.parseInt(champs[champs.length - 1].trim());
+
+	        File file = new File("src/assets/reservWeb/reserv.txt");
+	        File tempFile = new File("src/assets/reservWeb/reserv_temp.txt");
+
+	        try (BufferedReader reader = new BufferedReader(new FileReader(file));
+	             BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
+
+	            String lineRead;
+
+	            while ((lineRead = reader.readLine()) != null) {
+	                String[] parts = lineRead.split(" \\; ");
+
+	                if (parts.length >= 1) {
+	                    int id = Integer.parseInt(parts[0].trim());
+
+	                    // Écrivez la ligne dans le fichier temporaire uniquement si l'ID ne correspond pas à celui à supprimer
+	                    if (id != idToDelete) {
+	                        writer.write(lineRead);
+	                        writer.newLine();
+	                    }
+	                }
+	            }
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        if (file.delete()) {
+	            if (tempFile.renameTo(file)) {
+	            } 
+	        }
+
+	    }	
 	}
 }
